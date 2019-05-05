cab9d4964126515c689049de4878df2e9c258181
GlouBuf <eglantine.buffa@gmail.com>
2019-05-05 17:24:19 +0200

    version du 5 mai

--- src/pictures.py
+++ src/pictures.py
@@ -6,7 +6,10 @@ import utils
 import sys
 import time
 import exifread
+from _datetime import *
 from datetime import datetime, timezone, timedelta
+from configuration import *
+
 
 # TODO: Définir la classe PictureLocationProvider qui désigne des objets LocationProvider obtenus à partir d'images
 class PictureLocationProvider(ListLocationProvider):
@@ -14,12 +17,23 @@ class PictureLocationProvider(ListLocationProvider):
     def __init__(self, directory: str):
         self.__dir = str(directory)
         #samples doit contenir des couples (String: source_pic_file, LocationSample: sample) de sorte à garder une trace de l'origine de chaque objet LocationSample
-        samples = []
+        __samples = []
         # TODO: scanner le répertoire contenant les photos prises par le suspect et extraire un objet LocationSample pour les photos ayant une extension valide (et qui contiennent des informations de localisation)
+        liste_suffix = [".jpg", ".jpeg", ".JPG", ".JPEG"]
+        for fichier in os.scandir(directory) :
+            for suffix in liste_suffix:
+                if fichier.name.endswith(suffix):
+                    source_pic_file = str(fichier)
+                    elements_de_ls = PictureLocationProvider._extract_location_sample_from_picture(os.path.join(self.__dir, fichier.name)) #on obtient un datetime, une latitude et une longitude
+                    location_de_ls = Location(elements_de_ls[1], elements_de_ls[2])
+                    ls = LocationSample(elements_de_ls[0], location_de_ls) #on crée un LocationSample
+                    __samples.append([source_pic_file, ls])
+        super().__init__(__samples)
+
 
     # TODO: Définir les extensions valides et un getter pour y accéder
-    def get_list_valid_extensions():
-        pass
+    def get_list_valid_extensions(self):
+        return copy.deepcopy(self.__samples)
 
     # TODO: Redéfinir la méthode str pour afficher les objets sous la forme suivante :
     # PictureLocationProvider (source: ’ ../ data/pics /jdoe’ (JPG,JPEG,jpg,jpeg), 2 location samples)
@@ -51,8 +65,12 @@ class PictureLocationProvider(ListLocationProvider):
                     lng = -lng
 
             date = utils.get_if_exists(exif_data, 'GPS GPSDate')
+            ldate = str(date).split(':')
             timestamp = utils.get_if_exists(exif_data, 'GPS GPSTimeStamp')
+            ltimestamp = datetime.strptime(str(timestamp), "[%H, %M, %S]")
+            d = ltimestamp - datetime(1900, 1, 1)
             # TODO: Convertir la date (au format textuel) contenue dans le EXIF tag en datetime et le stocker dans la variable t
+            t = datetime(int(ldate[0]), int(ldate[1]), int(ldate[2])) + d
 
         return t, lat, lng
 
@@ -60,9 +78,9 @@ class PictureLocationProvider(ListLocationProvider):
 if __name__ == '__main__':
     pass
     # Tester l'implémentation de cette classe avec les instructions de ce bloc main (le résultat attendu est affiché ci-dessous)
-    #Configuration.get_instance().add_element("verbose", True)
-    #lp = PictureLocationProvider('../data/pics/michael')
-    #print(lp)
+    Configuration.get_instance().add_element("verbose", True)
+    lp = PictureLocationProvider('../data/pics/deray')
+    print(lp)
     #lp.show_location_samples()
     # Warning: Skipping file 'unil.jpg' (Missing time and/or location information)
     # PictureLocationProvider(source: '../data/pics/jdoe'(JPG, JPEG, jpg, jpeg), 2 location samples)
--- src/suspects.py
+++ src/suspects.py
@@ -8,17 +8,34 @@ from configuration import *
 
 # TODO: Définir la classe Suspect qui décrit des informations contenues dans un fichier (nom, LocationProvider)
 class Suspect:
+    #attribut de classe
+    list_suspects = []
     # TODO: Implémenter le constructeur et les getters
+    def __init__(self, name : str, sources):
+        self.__name = name
+        self.__sources = sources
+
+    def get_name(self):
+        return self.__name
+
+    def get_sources(self):
+        return self.__sources
+
 
     # TODO: Définir la méthode str pour afficher un Suspect de la manière suivante :
     # [Suspect] Name: jdoe, Location provider: PictureLocationProvider (source: ’ ../ data/pics /jdoe’ (JPG,JPEG,jpg,jpeg), 2 location samples)
 
     # TODO: Implémenter une méthode create_suspects_from_XML_file qui prend un nom de fichier XML en paramètre et le parse pour créer une liste de suspects
     # TODO: (Alternative) implémenter une méthode similaire pour les fichiers JSON
-    def create_suspects_from_XML_file():
-        pass
+    @classmethod
+    def create_suspects_from_XML_file(cls, filename):
+        # Lire un fichier xml et ajouter les suspects à la liste...
+        return cls.list_suspects
 
-    def create_suspects_from_JSON_file():
+    @classmethod
+    def create_suspects_from_JSON_file(cls, filename):
+        # Lire un fichier json et ajouter les suspects à la liste...
+        return cls.list_suspects
         pass
 
 
