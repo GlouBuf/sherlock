82f68b07a892f0003ad865bd74fc3b85a0af9db6
micbuffa <micbuffa@gmail.com>
2019-05-13 11:16:31 +0200

    Implemented get_surrounding_location_samples avec calcul dans la requête SQL, fichier wifi.py

--- src/wifi.py
+++ src/wifi.py
@@ -47,31 +47,62 @@ class WifiLogsLocationProvider(ListLocationProvider):
         pass
 
     def get_surrounding_temporal_location_sample(self, date : datetime):
-        pass
         db_wifi = sqlite3.connect(self.__db_filename)
         db_wifi.row_factory = utils.dict_factory  # pour utiliser les résultats sous forme de dict
         cursor = db_wifi.cursor()
         ts = datetime.timestamp(date)
+        #print("ts = ",ts)
 
         avant = None
-        for row in cursor.execute("SELECT timestamp, latitude, longitude from location_samples join users on location_samples.uid == users.id join hotspots on location_samples.hid == hotspots.id where users.name == '" + self.__username + "' and  location_samples.timestamp < '" + str(ts) + "'  order by location_samples.timestamp asc"):
-            print(row)
-        '''if rows.arraysize != 0 :
-            row_avant = rows[0]
+        # Technique SQL : on trie par ordre du plus récent au plus vieux (ordre DESC) et on ne garde que le premier (limit 1)
+        query = "SELECT timestamp, latitude, longitude from location_samples join users on \
+                location_samples.uid == users.id join hotspots on location_samples.hid == hotspots.id where users.name == '" + \
+                self.__username + "' and  location_samples.timestamp < '" + str(ts) + \
+                "' order by location_samples.timestamp desc limit 1"
+        #print(query)
+
+        row_avant = cursor.execute(query).fetchone()
+        if(row_avant != None):
             tstamp = row_avant['timestamp']
             date = datetime.fromtimestamp(tstamp)
             lat = row_avant['latitude']
             long = row_avant['longitude']
             avant = LocationSample(date, Location(lat, long))
-            print(avant)'''
+            #print("avant = ", avant)
+
+        # Technique SQL : on trie par ordre du plus viux au plus récent (ordre ASC) et on ne garde que le premier (limit 1)
+        query = "SELECT timestamp, latitude, longitude FROM location_samples JOIN users ON \
+                location_samples.uid == users.id JOIN hotspots ON location_samples.hid == hotspots.id WHERE users.name == '" + \
+                self.__username + "' AND  location_samples.timestamp > '" + str(ts) + \
+                "' ORDER BY location_samples.timestamp ASC LIMIT 1"
+        #print(query)
+
+        row_apres = cursor.execute(query).fetchone()
+        if(row_apres != None):
+            tstamp = row_apres['timestamp']
+            date = datetime.fromtimestamp(tstamp)
+            lat = row_apres['latitude']
+            long = row_apres['longitude']
+            apres = LocationSample(date, Location(lat, long))
+            #print("après = ", apres)
+
+
+        return (avant, apres)
 
 if __name__ == '__main__':
     pass
     # Tester l'implémentation de cette classe avec les instructions de ce bloc main (le résultat attendu est affiché ci-dessous)
     #Configuration.get_instance().add_element("verbose", True)
     lp = WifiLogsLocationProvider('../data/db/wifi.db', 'egravier')
-    print(lp)
-    print(lp.get_surrounding_temporal_location_sample(datetime(2018,5,16,11,27,23)))
-    lp.show_location_samples()
+    print(" --- Liste des Locations samples de egravier ---")
+    print(lp.print_location_samples())
+    print()
+    print("--- Test de get_surrounding_temporal_location_sample avec requête SQL qui fait le calcul... ---")
+    print("On teste avec une date du crime 2018-4-16 11:16:23")
+    print("Le bon résultat pour le LocationSample le plus proche AVANT et APRES cette date:")
+    plus_proches_ls = lp.get_surrounding_temporal_location_sample(datetime(2018,5,16,11,16,23))
+    print("Avant = ", plus_proches_ls[0])
+    print("Après = ", plus_proches_ls[1])
+    #lp.show_location_samples()
 
     # WifiLogsLocationProvider (source: '../data/db/wifi.db', user 'egravier', 4 location samples)
