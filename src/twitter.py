9b6b0b3e42a0e1ba049a8cc45535418d336c1d6a
GlouBuf <eglantine.buffa@gmail.com>
2019-05-19 16:03:13 +0200

    tweet

--- src/twitter.py
+++ src/twitter.py
@@ -10,11 +10,24 @@ from datetime import datetime, timezone, timedelta
 # TODO: Définir la classe TwitterLocationProvider
 class TwitterLocationProvider(ListLocationProvider):
     # TODO: Implémenter le constructeur où l'on construit une liste de LocationSample
-    def __init__(self, name : str, key, key_secret):
+    def __init__(self, name : str, token, token_secret):
         self.__name = name
-        self.set_api_key(key)
-        self.set_api_key_secret(key_secret)
+        self.__token = token
+        self.__token_secret = token_secret
+
         #todo : aller chercher les tweet et appeler extract location sample from tweet FAIRE COMME DANS PICTURES
+        consumer_key = TwitterLocationProvider.__api_key
+        consumer_secret = TwitterLocationProvider.__api_key_secret
+        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+
+        # set access token
+        auth.set_access_token(self.__token, self.__token_secret)
+
+        client = tweepy.API(auth)
+
+        user = client.me()  # obtenir les information sur l'utilisateur connecté
+        print(client, auth)
+
         self.__samples = []
         #self.__samples = self._extract_location_sample_from_tweet()
 
@@ -22,12 +35,16 @@ class TwitterLocationProvider(ListLocationProvider):
         # Appel du constructeur de la classe mere
         super().__init__(self.__samples)
 
-    # TODO: Définir des attributs pour les clefs de l'API ainsi que les setters correspondants
-    def set_api_key(self, key):
-        self.__api_key = key
 
-    def set_api_key_secret(self, key_secret):
-        self.__api_key_secret = key_secret
+
+    # TODO: Définir des attributs pour les clefs de l'API ainsi que les setters correspondants
+    @classmethod
+    def set_api_key(cls, key):
+        cls.__api_key = key
+
+    @classmethod
+    def set_api_key_secret(cls, key_secret):
+        cls.__api_key_secret = key_secret
 
 
     # TODO: Redéfinir la méthode str
@@ -37,17 +54,7 @@ class TwitterLocationProvider(ListLocationProvider):
     # TODO: Implémenter la méthode _extract_location_sample_from_tweet qui prend en paramètre un tweet et renvoie un tuple (temps, latitude, longitude)
     # Comme pour la méthode de Picture, vérifier que les paramètres sont bien présents dans le tweet
     def _extract_location_sample_from_tweet(self):
-        consumer_key = self.__api_key
-        consumer_secret = self.__api_key_secret
-        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-        # Get access token
-        #self.__access_token = None
-        #self.__access_token_secret = None
-        #auth.set_access_token(self.__access_token, self.__access_token_secret)
-        client = tweepy.API(auth)
 
-        user = client.me()  # obtenir les information sur l'utilisateur connecté
-        print(client, auth)
 
 
         return []
@@ -57,8 +64,13 @@ if __name__ == '__main__':
 
     # Tester l'implémentation de cette classe avec les instructions de ce bloc main (le résultat attendu est affiché ci-dessous)
     Configuration.get_instance().add_element("verbose", True)
-    #TwitterLocationProvider.set_api_key('Z4bLkruoqSp0JXJfJGTaMQEZo')
-    #TwitterLocationProvider.set_api_key_secret('gYyLCa7QiDje76VaTttlylDjGThCBGcp9MIcEGlzVq6FJcXIdc')
+    TwitterLocationProvider.set_api_key('Z4bLkruoqSp0JXJfJGTaMQEZo')
+    TwitterLocationProvider.set_api_key_secret('gYyLCa7QiDje76VaTttlylDjGThCBGcp9MIcEGlzVq6FJcXIdc')
+
+    lp = TwitterLocationProvider('1_isp','1124333850858074115-q2xK5TcnlRLGMk9QO1vMSi9RTcH6Sk','B8YQzoO01Dze2D3CaJLukuvXKRZn0VtSpPuCtYccdKYSZ')
+
+
+
     #lp = TwitterLocationProvider('egravier1994','996422909735391233-kLWsObS61ghUmpgS2ZXTZElQ2n5Tt9Z','BXqtUZI2pBUOgDyOVY21svUHyP8iabA98kdT4GpZlWWNC')
     #lp = TwitterLocationProvider('egravier1994', '996422909735391233-kLWsObS61ghUmpgS2ZXTZElQ2n5Tt9Z',
     #                             'BXqtUZI2pBUOgDyOVY21svUHyP8iabA98kdT4GpZlWWNC')
@@ -70,13 +82,12 @@ if __name__ == '__main__':
     # ensuite saisir pour que çà marche... et visiblement le code de tes profs
     # date de l'an dernier et je pense que cela a changé depuis....
     # demande moi ma clé secrete, il ne faut pas la mettre sur github
-    lp = TwitterLocationProvider('egravier1994', 'lSjQ9ctPFYbN4UwAAKPJ2PLIh',
-                                                              '')
+    #lp = TwitterLocationProvider('egravier1994', 'lSjQ9ctPFYbN4UwAAKPJ2PLIh',)                                                              '')
 
 
-    print(lp)
-    lp.print_location_samples()
-    lp.show_location_samples()
+    #print(lp)
+    #lp.print_location_samples()
+    #lp.show_location_samples()
 
     # Warning: Skipping tweet (Missing time and/or location information (996680525313200128))
     # TwitterLocationProvider (user 'egravier1994' aka 'Émile Gravier', 11 location samples)
